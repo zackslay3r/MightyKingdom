ad636ae39b121c99ad21f873675f5b457e1cb3ac
zackslay3r <zackslay3r25@gmail.com>
2019-12-11 22:52:47 +1100

    Added ground check and animation, as well as camera following.

---
+++ MightyKingdomPlatformer/Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public PlayerMovement player;
+
+    private Vector3 lastPlayerPos;
+    private float distanceToMoveCamera;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<PlayerMovement>();
+
+
+        lastPlayerPos = player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        distanceToMoveCamera = player.transform.position.x - lastPlayerPos.x;
+
+        transform.position = new Vector3(transform.position.x + distanceToMoveCamera, transform.position.y,transform.position.z);
+
+        lastPlayerPos = player.transform.position;
+    }
+}
--- MightyKingdomPlatformer/Assets/Scripts/PlayerMovement.cs
+++ MightyKingdomPlatformer/Assets/Scripts/PlayerMovement.cs
@@ -6,8 +6,13 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed;
     public float jumpPower;
-    private Rigidbody2D playerRb;
+    public bool onGround;
 
+
+    private Rigidbody2D playerRb;
+    private Collider2D playerCollider;
+    private Animator playerAnimator;
+    public LayerMask definedGround;
 
     
     // Start is called before the first frame update
@@ -15,21 +20,35 @@ public class PlayerMovement : MonoBehaviour
     {
         //Assign the value for playerRb
         playerRb = GetComponent<Rigidbody2D>();
-
-
+        // Assign the private 'playerCollider' variable.
+        playerCollider = GetComponent<Collider2D>();
+        // Assign the animator component
+        playerAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Determine if we are on the ground by checking if we are touchiing anything of the layer 'ground'
+        onGround = Physics2D.IsTouchingLayers(playerCollider, definedGround);
+
         // Have the player automatically move based on the speed that was predefined.
         playerRb.velocity = new Vector2(speed, playerRb.velocity.y);
 
         // If we hit the spacebar or tap the screen, then we want the player to jump
-        if (Input.GetKeyDown(KeyCode.Space) || Input.GetTouch(0).phase == TouchPhase.Began)
+        //if (Input.GetKeyDown(KeyCode.Space) || Input.GetTouch(0).phase == TouchPhase.Began)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            playerRb.velocity = new Vector2(0, jumpPower);
+            //If we are on the ground, allow the player the ability to be able to jump.
+            if (onGround)
+            {
+                playerRb.velocity = new Vector2(0, jumpPower);
+            }
         }
+
+
+        playerAnimator.SetFloat("Speed", playerRb.velocity.x);
+        playerAnimator.SetBool("Grounded", onGround);
 
     }
 }
