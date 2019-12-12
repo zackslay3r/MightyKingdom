a163f3f8a4dc4487f29e900408032c219c8e868d
zackslay3r <zackslay3r25@gmail.com>
2019-12-12 22:06:39 +1100

    Adjusted the camera settings, added changing UI text

---
+++ MightyKingdomPlatformer/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    // These are the two UI elemements that are within our Canvas.
+    public Text scoreText;
+    public Text highScoreText;
+
+    //These are the two values that will be used in conjunction with the two text elements above.
+    public float score;
+    public float highScore;
+
+    // We also need a variable for the points per second that we would like to give the player for each second they are alive.
+    public float scorePerSecond;
+
+    // We also want a boolean for when the player dies, stop increasing the score.
+    public bool increaseScore;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (score > highScore)
+        {
+            highScore = score;
+        }
+
+        scoreText.text = "Score: " + score;
+        highScoreText.text = "High Score: " + highScore;
+    }
+
+
+}
