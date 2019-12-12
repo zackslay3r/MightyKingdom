04e3fe03637f6a506a95a008145ef9d942ee9daf
zackslay3r <zackslay3r25@gmail.com>
2019-12-12 17:33:14 +1100

    Added the base of an object pool and added random platforms, now adding that the world moves instead of the player

---
+++ MightyKingdomPlatformer/Assets/Scripts/PlatformCreator.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformCreator : MonoBehaviour
+{
+    public GameObject[] platforms;
+    public Transform generationPoint;
+    public float distanceBetween;
+
+    //public ObjectPoolScript theObjectPool;
+
+    private float platformWidth;
+
+    private int platformIndex;
+    private float[] platformWidths;
+
+    // Two random floats that are able to determine the widith between platforms
+    public float pDistanceMin, pDistanceMax;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get the length of the platform that we are about to generate.
+        //platformWidth = newPlatform.GetComponent<BoxCollider2D>().size.x;
+        platformWidths = new float[platforms.Length];
+
+        for (int i = 0; i < platforms.Length; i++)
+        {
+            platformWidths[i] = platforms[i].GetComponent<BoxCollider2D>().size.x;
+
+        }
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < generationPoint.position.x)
+        {
+            distanceBetween = Random.Range(pDistanceMin, pDistanceMax);
+            
+            platformIndex = Random.Range(0, platforms.Length);
+            
+            transform.position = new Vector3(transform.position.x + platformWidths[platformIndex] + distanceBetween, transform.position.y, transform.position.z);
+
+            
+            
+            Instantiate(platforms[platformIndex], transform.position, transform.rotation);
+            //GameObject newPlatform = theObjectPool.getPooledObject();
+            /*
+            newPlatform.transform.position = transform.position;
+            newPlatform.transform.rotation = transform.rotation;
+            newPlatform.SetActive(true);
+            */
+        }
+    }
+}
---
+++ MightyKingdomPlatformer/Assets/Scripts/PlatformRemover.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformRemover : MonoBehaviour
+{
+    public GameObject platformDestroyPoint;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Find the destroy point of which we will delete platforms from.
+        platformDestroyPoint = GameObject.Find("DestroyPoint");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < platformDestroyPoint.transform.position.x)
+        {
+            //Destroy(gameObject);
+            gameObject.SetActive(false);
+        }
+    }
+}
